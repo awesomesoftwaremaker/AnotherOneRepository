c4c4734316e8f5984b98b3480fba798a7b41cbdc
awesomesoftwaremaker <FullMetalIceCream@gmail.com>
2018-09-23 17:05:55 +0600

    1.3 б

--- 1_3_b/main.cpp
+++ 1_3_b/main.cpp
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-int n = 4;
-
+const int n = 4;
 float A[n][n] = {1.7, 0.0003, 0.0004, 0.0005, 0.0, 0.8, 0.0001, 0.0002, -0.0003, -0.0002, -0.1, 0.0, -0.0005, -0.0004, -0.0003, -1.0};
-
 float B[n] = {0.681, 0.4803, -0.802, -1.0007};
-float X[30][n] = {0};
+float X[n][n] = {0};
 float TEST[n];
 float summ;
 float eps;
 int k = 0;
 
-//Условие окончания работы программы
+//Условие окончания итерации
 bool ExitFunc(float eps)
 {
     if(k == 0) return true;
@@ -25,9 +23,11 @@ bool ExitFunc(float eps)
 
 int main()
 {
-    printf("Enter eps:");//Взятие погрешности
+    printf("Enter eps:");
+    //Взятие погрешности
     scanf("%f", &eps);
     do {
+        //Процесс итерации
         for(int i = 0; i < n; i++) {
             summ = 0;
             for(int j = 0; j < n; j++)
@@ -38,17 +38,17 @@ int main()
         k++;
     } while(ExitFunc(eps));
 
-//Проверка
     printf("\nResult:\n");
     for(int j = 0; j < n; j++)
         printf("%4.8f ", X[k][j]);
 
+    //Проверка
     printf("\nTest:\n");
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++)
             TEST[i] += (A[i][j] * X[k][j]);
         printf("%4.8f ", TEST[i]);
     }
-
     printf("\nIterations:\n%i", k);
 }
+
